9cf261868252af5f0e8f09f6ddd15f4c32c1f982
victorinoLavida <victorinolavida@ciencias.unam.mx>
2024-10-27 14:23:11 -0600

    fix: fix colorschema set catpuchinno

--- lua/victorino/init.lua
+++ lua/victorino/init.lua
@@ -61,3 +61,5 @@ autocmd('LspAttach', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+print(vim.g.colorschema)
--- lua/victorino/lazy/colors.lua
+++ lua/victorino/lazy/colors.lua
@@ -1,17 +1,17 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
+    color = color or "catppuccin"
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-
-    {
-        "erikbackman/brightburn.vim",
-    },
-
+    --
+    -- {
+    --     "erikbackman/brightburn.vim",
+    -- },
+    --
     {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -20,70 +20,71 @@ return {
             ColorMyPencils()
         end
     },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
-    },
-
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-        end
-    },
+    -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     name = "gruvbox",
+    --     config = function()
+    --         require("gruvbox").setup({
+    --             terminal_colors = true, -- add neovim terminal colors
+    --             undercurl = true,
+    --             underline = false,
+    --             bold = true,
+    --             italic = {
+    --                 strings = false,
+    --                 emphasis = false,
+    --                 comments = false,
+    --                 operators = false,
+    --                 folds = false,
+    --             },
+    --             strikethrough = true,
+    --             invert_selection = false,
+    --             invert_signs = false,
+    --             invert_tabline = false,
+    --             invert_intend_guides = false,
+    --             inverse = true, -- invert background for search, diffs, statuslines and errors
+    --             contrast = "",  -- can be "hard", "soft" or empty string
+    --             palette_overrides = {},
+    --             overrides = {},
+    --             dim_inactive = false,
+    --             transparent_mode = false,
+    --         })
+    --     end,
+    -- },
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     config = function()
+    --         require("tokyonight").setup({
+    --             -- your configuration comes here
+    --             -- or leave it empty to use the default settings
+    --             style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    --             transparent = true,     -- Enable this to disable setting the background color
+    --             terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+    --             styles = {
+    --                 -- Style to be applied to different syntax groups
+    --                 -- Value is any valid attr-list value for `:help nvim_set_hl`
+    --                 comments = { italic = false },
+    --                 keywords = { italic = false },
+    --                 -- Background styles. Can be "dark", "transparent" or "normal"
+    --                 sidebars = "dark", -- style for sidebars, see below
+    --                 floats = "dark",   -- style for floating windows
+    --             },
+    --         })
+    --     end
+    -- },
+    --
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     config = function()
+    --         require('rose-pine').setup({
+    --             disable_background = true,
+    --             styles = {
+    --                 italic = false,
+    --             },
+    --         })
+    --     end
+    -- },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 
 
 }
