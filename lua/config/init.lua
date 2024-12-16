d250c45d1ea636e7142bacddaa83770bcef4c6bf
victorinoLavida <victorinolavida@ciencias.unam.mx>
2024-12-15 22:49:14 -0600

    fix: go to implementations keymap

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -21,6 +21,8 @@ autocmd('LspAttach', {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
