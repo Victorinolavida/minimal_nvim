5a2c54b4ff768601fa4193a2c53f1ab78de3cbc6
victorinoLavida <victorinolavida@ciencias.unam.mx>
2025-04-11 19:46:41 -0600

    "fix: delete bg in keywords"

--- lua/config/plugins/cloack.lua
+++ lua/config/plugins/cloack.lua
@@ -1,29 +1,34 @@
 return {
-    "laytan/cloak.nvim",
-    config = function()
-        require("cloak").setup({
-            enabled = true,
-            cloak_character = "*",
-            -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
-            highlight_group = "Comment",
-            patterns = {
-                {
-                    -- Match any file starting with ".env".
-                    -- This can be a table to match multiple file patterns.
-                    file_pattern = {
-                        ".env*",
-                        "wrangler.toml",
-                        ".dev.vars",
-                    },
-                    -- Match an equals sign and any character after it.
-                    -- This can also be a table of patterns to cloak,
-                    -- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
-                    cloak_pattern = "=.+"
-                },
-            },
-        })
+	"laytan/cloak.nvim",
+	config = function()
+		require("cloak").setup({
+			enabled = true,
+			cloak_character = "*",
+			-- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+			-- highlight_group = "Comment",
+			try_all_patterns = true,
+			patterns = {
+				{
+					-- Match any file starting with ".env".
+					-- This can be a table to match multiple file patterns.
+					file_pattern = {
+						".env*",
+						"wrangler.toml",
+						".dev.vars",
+						"config.yaml",
+					},
+					-- Match an equals sign and any character after it.
+					-- This can also be a table of patterns to cloak,
+					-- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
+					-- cloak_pattern = { "=.+", ":-.+" },
+					cloak_pattern = {
+						"=.+", -- for key=value style
+						":[ ]?.+", -- for YAML: key: value style
+					},
+				},
+			},
+		})
 
-        vim.keymap.set('n', '<leader>cd', ":CloakToggle<CR>", { desc = 'Toggle cloak' })
-    end
+		vim.keymap.set("n", "<leader>cd", ":CloakToggle<CR>", { desc = "Toggle cloak" })
+	end,
 }
-
