87dfd8b61b409c162e32c49bc3dc0faa9a19ad09
victorinoLavida <victorinolavida@ciencias.unam.mx>
2025-04-11 19:20:54 -0600

    fix: add .devbox to ignored path

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -1,118 +1,120 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false,
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        require("nvim-tree").setup({
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	config = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		require("nvim-tree").setup({
 
-            sync_root_with_cwd = true,
-            -- hijack_unnamed_buffer_when_opening = false,
-            update_focused_file = {
-                enable = true,
-                update_cwd = true,
-                ignore_list = {},
-            },
+			sync_root_with_cwd = true,
+			-- hijack_unnamed_buffer_when_opening = false,
+			update_focused_file = {
+				enable = true,
+				update_cwd = true,
+				ignore_list = {},
+			},
 
-            actions = {
-                expand_all = {
-                    exclude = {
-                        "node_modules",
-                        ".git",
-                        ".cache",
-                        ".vscode",
-                        ".idea",
-                        ".DS_Store",
-                        ".gitignore",
-                        ".gitattributes",
-                        ".gitmodules",
-                        ".gitkeep",
-                        ".gitlab-ci.yml",
-                        ".gitlab",
-                        "__pycache__",
-                    },
-                },
-            },
-            hijack_netrw = true,
-            hijack_cursor = true,
-            diagnostics = {
-                enable = true,
-                icons = {
-                    hint = "",
-                    info = "",
-                    warning = "",
-                    error = "",
-                },
-            },
+			actions = {
+				expand_all = {
+					exclude = {
+						"node_modules",
+						".devbox",
+						".git",
+						".cache",
+						".vscode",
+						".idea",
+						".DS_Store",
+						".gitignore",
+						".gitattributes",
+						".gitmodules",
+						".gitkeep",
+						".gitlab-ci.yml",
+						".gitlab",
+						"__pycache__",
+					},
+				},
+			},
+			hijack_netrw = true,
+			hijack_cursor = true,
+			diagnostics = {
+				enable = true,
+				icons = {
+					hint = "",
+					info = "",
+					warning = "",
+					error = "",
+				},
+			},
 
-            filters = {
-                custom = { "^.git$" },
-            },
-            git = {
-                enable = true,
-            },
-            filesystem_watchers = {
-                ignore_dirs = {
-                    "node_modules",
-                    ".git",
-                    ".cache",
-                    ".vscode",
-                    ".idea",
-                    ".DS_Store",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                    ".gitkeep",
-                    ".gitlab-ci.yml",
-                    ".gitlab",
-                    "__pycache__",
-                },
-            },
+			filters = {
+				custom = { "^.git$" },
+			},
+			git = {
+				enable = true,
+			},
+			filesystem_watchers = {
+				ignore_dirs = {
+					"node_modules",
+					".git",
+					".cache",
+					".vscode",
+					".idea",
+					".DS_Store",
+					".gitignore",
+					".gitattributes",
+					".gitmodules",
+					".gitkeep",
+					".gitlab-ci.yml",
+					".gitlab",
+					"__pycache__",
+					".devbox",
+				},
+			},
 
-            renderer = {
-                root_folder_label = false,
-                highlight_git = true,
-                indent_markers = { enable = true },
-                icons = {
-                    show = {
-                        file = true,
-                        folder = true,
-                        folder_arrow = true,
-                        git = true,
-                    },
-                    glyphs = {
-                        default = "󰈚",
-                        folder = {
-                            default = "",
-                            empty = "",
-                            empty_open = "",
-                            open = "",
-                            symlink = "",
-                        },
-                        git = {
-                            -- modified = "", -- Archivo modificado
-                            -- removed = "", -- Archivo eliminado
-                            renamed = "", -- Archivo renombrado
-                            untracked = "", -- Archivo no trackeado
-                            ignored = "", -- Archivo ignorado por .gitignore
-                            staged = "", -- Archivo en stage listo para commit
-                            unmerged = "",
-                            -- conflict = "", -- Conflicto de merge
-                        },
-                    },
-                },
-            },
-        })
-        --       local api = require "nvim-tree.api"
-        -- api.events.subscribe(api.events.Event.FileCreated, function(file)
-        -- 	vim.cmd("edit " .. file.fname)
-        -- end)
+			renderer = {
+				root_folder_label = false,
+				highlight_git = true,
+				indent_markers = { enable = true },
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+					glyphs = {
+						default = "󰈚",
+						folder = {
+							default = "",
+							empty = "",
+							empty_open = "",
+							open = "",
+							symlink = "",
+						},
+						git = {
+							-- modified = "", -- Archivo modificado
+							-- removed = "", -- Archivo eliminado
+							renamed = "", -- Archivo renombrado
+							untracked = "", -- Archivo no trackeado
+							ignored = "", -- Archivo ignorado por .gitignore
+							staged = "", -- Archivo en stage listo para commit
+							unmerged = "",
+							-- conflict = "", -- Conflicto de merge
+						},
+					},
+				},
+			},
+		})
+		--       local api = require "nvim-tree.api"
+		-- api.events.subscribe(api.events.Event.FileCreated, function(file)
+		-- 	vim.cmd("edit " .. file.fname)
+		-- end)
 
-        vim.keymap.set("n", "<leader>pv", function()
-            vim.cmd("NvimTreeToggle")
-        end, { noremap = true, silent = true, desc = "Toggle [P]roject [V]iew" })
-    end,
+		vim.keymap.set("n", "<leader>pv", function()
+			vim.cmd("NvimTreeToggle")
+		end, { noremap = true, silent = true, desc = "Toggle [P]roject [V]iew" })
+	end,
 }
