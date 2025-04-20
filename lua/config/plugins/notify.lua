22639511ecd9b1ce3d0d023656a00baec9e2dfdf
victorinoLavida <victorinolavida@ciencias.unam.mx>
2025-04-19 21:18:00 -0600

    feat: add notification plugin

---
+++ lua/config/plugins/notify.lua
@@ -0,0 +1,41 @@
+return {
+
+	"rcarriga/nvim-notify",
+	event = "VeryLazy",
+	config = function(_, opts)
+		local notify = require("notify")
+		notify.setup(opts)
+
+		-- Override default vim.notify
+		vim.notify = notify
+
+		-- Set notify as the default handler for LSP
+		require("lspconfig.ui.windows").default_options = {
+			border = "rounded",
+			relative = "cursor",
+			style = "minimal",
+			zindex = 50,
+			top_down = true,
+			focusable = false,
+			anchor = "SW",
+			row = 0,
+			col = 1,
+			position = "top",
+		}
+
+		-- notificcation center
+		vim.keymap.set("n", "<leader>n", function()
+			notify.dismiss({ silent = true })
+		end, {
+			desc = "Dismiss all notifications",
+		})
+		-- notification center
+		vim.keymap.set("n", "<leader>nc", function()
+			vim.cmd("Notifications")
+		end, {
+			desc = "Notification Center",
+		})
+	end,
+	-- Optional: Customize the appearance of the notifications
+	--
+}
