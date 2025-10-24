ef48c9bd0e85d9dc2cc2bbaec2773172c122361a
Victorino Lavida <victorino.lavida@mtrade.mx>
2025-10-24 09:28:52 -0600

    fix: auto session keymaps

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -10,7 +10,7 @@ return {
 
 		local keymap = vim.keymap
 
-		keymap.set("n", "<leader>wr", "<cmd>AutoSession restore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-		keymap.set("n", "<leader>ws", "<cmd>AutoSession save<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+		keymap.set("n", "<leader>ar", "<cmd>AutoSession restore<CR>", { desc = "Restore session for cwd" })       -- restore last workspace session for current directory
+		keymap.set("n", "<leader>as", "<cmd>AutoSession save<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
 	end,
 }
