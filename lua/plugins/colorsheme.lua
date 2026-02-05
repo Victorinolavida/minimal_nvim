8eb5f0a7ab31e7ed98afe68827e2b7e3846c7a00
victorinoLavida <victorinolavida@ciencias.unam.mx>
2026-02-04 22:20:52 -0600

    chore: delete koda colorschema

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -97,14 +97,14 @@ return {
 		end, -- Default options:
 	},
 	-- KODA
-	{
-		"oskarnurm/koda.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		name = "koda",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- require("koda").setup({ transparent = true })
-			vim.cmd("colorscheme koda")
-		end,
-	},
+	-- {
+	-- 	"oskarnurm/koda.nvim",
+	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	-- 	name = "koda",
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		-- require("koda").setup({ transparent = true })
+	-- 		vim.cmd("colorscheme koda")
+	-- 	end,
+	-- },
 }
