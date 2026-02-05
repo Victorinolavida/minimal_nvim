b095f74aba0c3635a0c0a8ae202ab8ab95a7d05f
victorinoLavida <victorinolavida@ciencias.unam.mx>
2026-02-04 23:29:10 -0600

    feat add new project config

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,37 +1,23 @@
 return {
-	"coffebar/neovim-project",
+	"victorinolavida/neovim-project",
+	branch = "feature/depth-based-project-discovery",
 	opts = {
 		projects = { -- define project roots
-			"~/Documents/dev/*",
-			"~/.config/*",
-		},
-		picker = {
-			type = "telescope", -- one of "telescope", "fzf-lua", or "snacks"
+			{ path = "~/Documents/", depth = 4 },
 		},
 	},
-	init = function()
-		-- enable saving the state of plugins in the session
-		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-	end,
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
-		-- optional picker
-		{ "nvim-telescope/telescope.nvim", tag = "0.1.4" },
-		-- optional picker
-		{ "ibhagwan/fzf-lua" },
-		-- optional picker
-		{ "folke/snacks.nvim" },
+		{ "nvim-telescope/telescope.nvim" },
 		{ "Shatur/neovim-session-manager" },
 	},
+	-- config = function()
+	-- 	vim.keymap.set("n", "<leader>pf", function()
+	-- 		vim.cmd("NeovimProjectDiscover")
+	-- 	end, {
+	-- 		desc = "[P]roject [F]ind",
+	-- 	})
+	-- end,
 	lazy = false,
 	priority = 100,
-	config = function()
-		local project_manager = require("neovim-project")
-
-		vim.keymap.set("n", "<leader>pf", function()
-			vim.cmd("NeovimProjectDiscover")
-		end, {
-			desc = "[P]roject [F]ind",
-		})
-	end,
 }
